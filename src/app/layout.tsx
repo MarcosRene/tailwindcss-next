de16e2d6e95812e6711db65fd8c49f71393dd073
Marcos Rene <renemoura789@gmail.com>
2024-04-07 18:00:15 -0300

    feat: add theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from 'next-themes'
+
 import './globals.css'
 
 import { Sidebar } from '@/components/Sidebar'
@@ -16,15 +18,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className="dark antialiased">
+    <html lang="en" className="antliased dark" suppressHydrationWarning>
       <body className={inter.className}>
-        <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
-          <Sidebar />
+        <ThemeProvider attribute="class">
+          <div className="min-h-screen lg:grid lg:grid-cols-app dark:bg-zinc-900">
+            <Sidebar />
 
-          <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
-            {children}
-          </main>
-        </div>
+            <main className="max-w-[100vw] px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+              {children}
+            </main>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
 import { Cog, LifeBuoy, Menu, Search } from 'lucide-react'
 
 import * as Collapsible from '@radix-ui/react-collapsible'
@@ -13,16 +16,45 @@ import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Button } from '../Button'
 
 export function Sidebar() {
+  const [mounted, setMounted] = useState(false)
+  const { setTheme, resolvedTheme } = useTheme()
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return
+
   return (
     <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white p-4 data-[state=open]:bottom-0 lg:right-auto lg:w-80 lg:border-r lg:px-5 lg:py-8 lg:data-[state=closed]:bottom-0 dark:border-zinc-800 dark:bg-zinc-900">
       <div className="flex items-center justify-between">
         <Logo />
 
-        <Collapsible.Trigger asChild className="lg:hidden">
-          <Button variant="ghost">
-            <Menu className="size-6" />
-          </Button>
-        </Collapsible.Trigger>
+        <div className="flex gap-4">
+          {resolvedTheme === 'dark' ? (
+            <Button
+              variant="ghost"
+              aria-label="Sun"
+              onClick={() => setTheme('light')}
+            >
+              <img src="sun.png" alt="Sun Icon" className="size-6" />
+            </Button>
+          ) : (
+            <Button
+              variant="ghost"
+              aria-label="Moon"
+              onClick={() => setTheme('dark')}
+            >
+              <img src="moon.png" alt="Moon Icon" className="size-6" />
+            </Button>
+          )}
+
+          <Collapsible.Trigger asChild className="lg:hidden">
+            <Button variant="ghost">
+              <Menu className="size-6" />
+            </Button>
+          </Collapsible.Trigger>
+        </div>
       </div>
 
       <Collapsible.Content
