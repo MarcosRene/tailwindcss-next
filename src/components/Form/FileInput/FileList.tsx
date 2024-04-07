ca2b8b8d350fa096976b0cf26599a2e6ce1f08d1
Marcos Rene <renemoura789@gmail.com>
2024-04-07 13:05:44 -0300

    feat: add animations

--- src/components/Form/FileInput/FileList.tsx
+++ src/components/Form/FileInput/FileList.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { Trash2, UploadCloud } from 'lucide-react'
 import { useFileInput } from './Root'
 
@@ -7,9 +8,10 @@ import { formatBytes } from '@/utils/formatBytes'
 
 export function FileList() {
   const { files } = useFileInput()
+  const [parent] = useAutoAnimate()
 
   return (
-    <div className="mt-4 space-y-3">
+    <div ref={parent} className="mt-4 space-y-3">
       {files.map((file) => (
         <div
           key={file.name}
